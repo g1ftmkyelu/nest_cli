231e3837d36f110bbef5ef0be9082f3afc9b9ecc
g1ftmkyelu <mkyelugift@gmail.com>
2025-02-14 09:07:25 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { program } from 'commander';
+import fs from 'fs/promises';
+import path from 'path';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// If nest_cli.js is in the src folder
+import './src/nest_cli.js';  // Import the whole file, or you can use specific exports if needed.
+
